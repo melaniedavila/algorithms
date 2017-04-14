b45e79f9fa762f5f60994c0c04f1c4007124ccc6
Melanie Davila <davilam91@gmail.com>
2017-04-13 21:01:46 -0400

    Add example rectangle

--- interview-cake/06-rectangular-love.rb
+++ interview-cake/06-rectangular-love.rb
@@ -1,4 +1,16 @@
+# rectangle = {
+#
+#     # bottom-left corner
+#     'left_x' => 1,
+#     'bottom_y' => 5,
+#
+#     # width and height
+#     'width' => 10,
+#     'height' => 4,
+#
+# }
+
 def rectangular_love(rect1, rect2)
-  
+
 
 end
