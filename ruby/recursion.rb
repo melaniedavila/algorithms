0f2583cc152804c73b094894f86b4a84640baec8
Melanie Davila <davilam91@gmail.com>
2017-04-18 19:22:58 -0400

    Complete recursive range problem

--- ruby/recursion.rb
+++ ruby/recursion.rb
@@ -7,3 +7,10 @@ def sum(arr)
   return 0 if arr.empty?
   sum(arr[0..-2]) + arr.last
 end
+
+# inclusive of end
+def range(start_num, end_num)
+  return [] if end_num < start_num
+
+  range(start_num, end_num - 1) << end_num
+end
